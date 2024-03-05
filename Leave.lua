01e6af6df2381ff551de3da4466a9f71209f1745
johan-sorman <johan.sorman@gmail.com>
2024-03-05 15:57:49 +0100

    Cleaning up some of the orginal code and adding a cmd

--- Leave.lua
+++ Leave.lua
@@ -24,32 +24,33 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ]]
 
 _addon.name = 'Leave'
-_addon.author = 'Daneblood'
-_addon.version = '23.4.20'
+_addon.author = 'Daneblood, Updated by Melu'
+_addon.version = '5.3.24'
 _addon.command = 'Leave'
 
 require('resources')
 res = require('resources')
 
-
-
-
+local chatColor = 207
 
 windower.register_event('addon command', function(...)
     local args = T{...}
     local cmd = args[1]
-	local var_thiszone = windower.ffxi.get_info()['zone']
+	local zone = windower.ffxi.get_info()['zone']
 
-    if cmd == 'all' then
+	if cmd == 'exit' then
+		windower.send_command('Leave')
+
+    elseif cmd == 'all' then
         windower.send_ipc_message('Leave_all')
 	end
 
 
-	if var_thiszone == 55 then  -- Assaults
+	if zone == 55 then  -- Assaults
 		greeting()
 		windower.send_command('Input /item "Reef Fireflies" <me>')
 
-	elseif var_thiszone == 73 then -- Zhayolm Remnants
+	elseif zone == 73 then -- Zhayolm Remnants
 		greeting()
 		drop_item(5365)
 		drop_item(5366)
@@ -72,7 +73,7 @@ windower.register_event('addon command', function(...)
 		drop_item(5383)
 		windower.send_command('Input /item "z. rem. fireflies" <me>')
 
-	elseif var_thiszone == 74 then -- Arrapago Remnants
+	elseif zone == 74 then -- Arrapago Remnants
 		greeting()
 		drop_item(5365)
 		drop_item(5366)
@@ -95,7 +96,7 @@ windower.register_event('addon command', function(...)
 		drop_item(5383)
 		windower.send_command('Input /item "a. rem. fireflies" <me>')
 
-	elseif var_thiszone == 75 then -- Bhaflau Remnants
+	elseif zone == 75 then -- Bhaflau Remnants
 		greeting()
 		drop_item(5365)
 		drop_item(5366)
@@ -118,7 +119,7 @@ windower.register_event('addon command', function(...)
 		drop_item(5383)
 		windower.send_command('Input /item "b. rem. fireflies" <me>')
 
-	elseif var_thiszone == 76 then -- Silver Sea Remnants
+	elseif zone == 76 then -- Silver Sea Remnants
 		greeting()
 		drop_item(5365)
 		drop_item(5366)
@@ -142,35 +143,35 @@ windower.register_event('addon command', function(...)
 		windower.send_command('Input /item "s. rem. fireflies" <me>')
 
 
-	elseif var_thiszone == 78 then -- Einhejar
+	elseif zone == 78 then -- Einhejar
 		greeting()
 		drop_item(5414)
 
-	elseif var_thiszone == 129 then -- Meeble Burrows
+	elseif zone == 129 then -- Meeble Burrows
 		greeting()
 		windower.send_command('Input /item "Hiatus Whistle" <me>')
 
-	elseif var_thiszone == 133 then -- Sortie
+	elseif zone == 133 then -- Sortie
 		greeting()
 		windower.send_command('Input /item "Obsid. Wing" <me>')
 
-	elseif var_thiszone == 271 then -- Incursion & Delve
+	elseif zone == 271 then -- Incursion & Delve
 		greeting()
 		windower.send_command('Input /item "Ontic Extremity" <me>')
 
-	elseif var_thiszone == 275 or var_thiszone == 189 then -- Vagary
+	elseif zone == 275 or zone == 189 then -- Vagary
 		greeting()
 		windower.send_command('Input /item "Ontic Extremity" <me>')
 
-	elseif var_thiszone == 292 then
+	elseif zone == 292 then
 		greeting()
 		windower.send_command('Input /item "reisenjima cage" <me>')
 	
-	elseif var_thiszone == 294 or var_thiszone == 295 or var_thiszone == 296 or var_thiszone == 297 then -- Dynamis Divergence
+	elseif zone == 294 or zone == 295 or zone == 296 or zone == 297 then -- Dynamis Divergence
 		greeting()
 		windower.send_command('Input /item "Black hourglass " <me>')
 
-	elseif var_thiszone == 298 then -- Odyssey & HTMB: A Stygian Pact, Champion of the Dawn, Divine Interference, Maiden of the Dusk
+	elseif zone == 298 then -- Odyssey & HTMB: A Stygian Pact, Champion of the Dawn, Divine Interference, Maiden of the Dusk
 		greeting()
 		windower.send_command('Input /item "moglophone II" <me>') -- Odyssey
 		windower.send_command('Input /item "V. Con. Shard" <me>') -- HTMB: A Stygian Pact, Champion of the Dawn, Divine Interference, Maiden of the Dusk
@@ -178,11 +179,7 @@ windower.register_event('addon command', function(...)
 	else
 		windower.add_to_chat(chatColor, 'This location is unknown. Unable to leave')
 	end
-
-
 end)
-
-
 
 windower.register_event('ipc message',function (msg)
     if msg == 'Leave_all' then
@@ -190,45 +187,10 @@ windower.register_event('ipc message',function (msg)
     end
 end)
 
-
-
 function greeting()
 	windower.send_command('tr passall"')
-	windower.send_command('roller off"')
-	windower.send_command('cpaddon cmd stop"')
-
-	local var_random = math.random(1,14)
-	if var_random == 1 then
-		windower.add_to_chat(chatColor, 'See you later, Alligator')
-	elseif var_random == 2 then
-		windower.add_to_chat(chatColor, 'Gotta run, Skeleton')
-	elseif var_random == 3 then
-		windower.add_to_chat(chatColor, 'See you soon, Baboon')
-	elseif var_random == 4 then
-		windower.add_to_chat(chatColor, 'Gotta go, Buffalo')
-	elseif var_random == 5 then
-		windower.add_to_chat(chatColor, 'Out the door, Dinosaur')
-	elseif var_random == 6 then
-		windower.add_to_chat(chatColor, 'Chop chop, Lolipop')
-	elseif var_random == 7 then
-		windower.add_to_chat(chatColor, 'Better swish, Jellyfish')
-	elseif var_random == 8 then
-		windower.add_to_chat(chatColor, 'Bye bye, Butterfly')
-	elseif var_random == 9 then
-		windower.add_to_chat(chatColor, 'Toodle-loo, Kangaroo')
-	elseif var_random == 10 then
-		windower.add_to_chat(chatColor, 'Blow a kiss, Goldfish')
-	elseif var_random == 11 then
-		windower.add_to_chat(chatColor, 'Goodbye hug, LadyBug')
-	elseif var_random == 12 then
-		windower.add_to_chat(chatColor, 'Hit the trail, Tiny Snail')
-	elseif var_random == 13 then
-		windower.add_to_chat(chatColor, 'Gotta scram, Litle Lamb')
-	elseif var_random == 14 then
-		windower.add_to_chat(chatColor, 'Take care, Polar Bear')
-	end
+	-- windower.add_to_chat(chatColor, 'See you later, Alligator')
 end
-
 
 function drop_item(var_item)
     local inventory = windower.ffxi.get_items(0)
